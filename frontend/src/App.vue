92bd8597ca81a8b8cdf0654cb592f29c8395d2d2
affciak <patryk.jarzynka1234@gmail.com>
2024-06-18 14:08:48 +0200

    Landing page v.01

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+
+</script>
+
+<template>
+  <v-app>
+    <Header/>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+@import '@/utils/colors.scss';
+
+.v-main {
+  background-color: $companyBackgroundColor;
+}
+
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {HEADER_HEIGHT} from "@/utils/constants";
+
+const headerItems = [
+    'STRONA GŁÓWNA',
+    'OFERTA',
+    'INFORMACJE'
+]
+
+</script>
+
+<template>
+  <v-app-bar
+      class="position-fixed"
+      :height="HEADER_HEIGHT"
+  >
+    <div class="header-container">
+      <v-row justify="center" class="ma-0 h-100">
+        <div v-for="item in headerItems">
+          <v-btn
+              flat
+              class="header-item h-100">
+            <p>
+              {{item}}
+            </p>
+          </v-btn>
+        </div>
+      </v-row>
+    </div>
+  </v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+@import '../utils/colors.scss';
+
+  .header-container {
+    width: 100%;
+    height: 100%;
+    border-bottom: 0.125rem solid $primaryColor;
+  }
+
+  .header-item {
+    p {
+      font-weight: 700;
+      color: $primaryColor;
+    }
+  }
+
+  .logo-img {
+    width: 450px;
+    position: absolute;
+    height: 100%;
+    left: 0;
+  }
+
+</style>
