d80388f03d3495f2abc2a5067b96a3aded130f6d
affciak <patryk.jarzynka1234@gmail.com>
2024-06-18 15:49:17 +0200

    Implemented temporary car items

--- frontend/src/views/Oferta.vue
+++ frontend/src/views/Oferta.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import {temporaryCarItems} from "@/utils/temporary-car-items";
 </script>
 
 <template>
@@ -7,7 +8,26 @@
       <h1 class="main-title">{{'OFERTA'}}</h1>
 
       <div class="offer-items-container">
+        <v-card v-for="car in temporaryCarItems">
+            <v-img
+                :src="car.img"
+                :height="400"
+                cover
+            />
 
+          <v-card-title>{{car.title}}</v-card-title>
+
+          <div class="card-info">
+            <v-card-subtitle>{{car.rocznik}}</v-card-subtitle>
+            <v-card-subtitle>{{car.przebieg + ' km'}}</v-card-subtitle>
+            <v-card-subtitle>{{car.paliwo}}</v-card-subtitle>
+            <v-card-subtitle>{{car.moc + 'KM'}}</v-card-subtitle>
+            <v-card-subtitle>{{car.pojemnosc + ' cm3'}}</v-card-subtitle>
+          </div>
+
+          <v-card-text class="price">4000PLN</v-card-text>
+
+        </v-card>
       </div>
     </v-container>
 </template>
@@ -33,5 +53,21 @@
     display: grid;
     width: 100%;
     flex: 1;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 30px;
+  }
+
+  .card-info {
+    display: flex;
+
+    .v-card-subtitle {
+      font-size: 15px;
+    }
+  }
+
+  .price {
+    font-size: 22px;
+    font-weight: 500;
+    color: $accentColor;
   }
 </style>
