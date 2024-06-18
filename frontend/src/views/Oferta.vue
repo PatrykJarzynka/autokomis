c7da4ab78b1c0b1beca3cb55f25313e0e18b4852
affciak <patryk.jarzynka1234@gmail.com>
2024-06-18 14:46:30 +0200

    Implemented new offer view

--- frontend/src/views/Oferta.vue
+++ frontend/src/views/Oferta.vue
@@ -3,9 +3,35 @@
 </script>
 
 <template>
-  $END$
+    <v-container class="offer-container">
+      <h1 class="main-title">{{'OFERTA'}}</h1>
+
+      <div class="offer-items-container">
+
+      </div>
+    </v-container>
 </template>
 
 <style scoped lang="scss">
+@import '../utils/colors.scss';
 
+  .offer-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+  }
+
+  .main-title {
+    font-size: 40px;
+    font-weight: 500;
+    color: $primaryColor;
+    height: fit-content;
+  }
+
+  .offer-items-container {
+    display: grid;
+    width: 100%;
+    flex: 1;
+  }
 </style>
